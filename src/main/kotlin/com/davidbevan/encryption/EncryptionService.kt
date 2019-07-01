d97675b5562d5fd39518ef7f961b9da51ccf2818
dabevan <dabevan@gmail.com>
2019-07-01 14:48:45 +0100

    Add some more openssl tests and increase level of comments on tests

--- src/main/kotlin/com/davidbevan/encryption/EncryptionService.kt
+++ src/main/kotlin/com/davidbevan/encryption/EncryptionService.kt
@@ -1,21 +1,32 @@
 package com.davidbevan.encryption
 
+import com.google.cloud.kms.v1.AsymmetricDecryptRequest
 import com.google.cloud.kms.v1.CryptoKeyName
+import com.google.cloud.kms.v1.CryptoKeyVersionName
 import com.google.cloud.kms.v1.KeyManagementServiceClient
 import com.google.protobuf.ByteString
 import org.bouncycastle.util.encoders.Base64
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PublicKey
 import javax.crypto.Cipher
 import javax.crypto.CipherInputStream
 import javax.crypto.CipherOutputStream
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
-import com.google.cloud.kms.v1.AsymmetricDecryptResponse
-import com.google.cloud.kms.v1.AsymmetricDecryptRequest
-import com.google.cloud.kms.v1.CryptoKeyVersionName
-
-
+import java.security.KeyFactory
+import java.security.spec.X509EncodedKeySpec
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.InvalidKeySpecException
+import java.security.NoSuchAlgorithmException
+import java.io.IOException
+import java.nio.file.Files
+import java.security.PrivateKey
+import java.nio.file.Files.readAllBytes
+import java.nio.file.Path
+import java.nio.file.Paths
 
 
 class EncryptionService(val projectId: String, val locationId: String, val keyRingId: String, val cryptoKeyId: String) {
@@ -42,11 +53,11 @@ class EncryptionService(val projectId: String, val locationId: String, val keyRi
     fun asymmetricDecrypt(encryptedText: ByteArray): ByteArray {
         KeyManagementServiceClient.create().use { keyManagementServiceClient ->
             val name = CryptoKeyVersionName.of(
-                    projectId,
-                    locationId,
-                    keyRingId,
-                    cryptoKeyId,
-                    "1"
+                projectId,
+                locationId,
+                keyRingId,
+                cryptoKeyId,
+                "1"
             )
             val ciphertext = ByteString.copyFrom(encryptedText)
             val request = AsymmetricDecryptRequest.newBuilder()
@@ -88,5 +99,75 @@ class AESEncryption(private val secretKey: SecretKey) {
         }
     }
 
+
+}
+
+
+class AsymmetricEncryptionService() {
+
+//rsa_padding_mode:oaep
+//rsa_oaep_md:sha256
+//rsa_mgf1_md:sha256
+    fun encrypt(data :ByteArray, publicKeyString: String): ByteArray {
+        val publicKey: PublicKey = getKey(publicKeyString)!!
+        val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-512AndMGF1Padding")
+        //val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding")
+        //val cipher = Cipher.getInstance("RSA")
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        return cipher.doFinal(data)
+    }
+
+
+    fun getKey(key: String): PublicKey? {
+        try {
+            val byteKey = Base64.decode(key)
+            val X509publicKey = X509EncodedKeySpec(byteKey)
+            val kf = KeyFactory.getInstance("RSA")
+
+            return kf.generatePublic(X509publicKey)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+}
+
+
+
+class EncryptionUsingExportedOpenSSLKeys {
+    @Throws(IOException::class)
+    fun readFileBytes(filename: String): ByteArray {
+        val path = Paths.get(filename)
+        return Files.readAllBytes(path)
+    }
+
+    @Throws(IOException::class, NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+    fun readPublicKey(filename: String): PublicKey {
+        val publicSpec = X509EncodedKeySpec(readFileBytes(filename))
+        val keyFactory = KeyFactory.getInstance("RSA")
+        return keyFactory.generatePublic(publicSpec)
+    }
+
+    @Throws(IOException::class, NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+    fun readPrivateKey(filename: String): PrivateKey {
+        val keySpec = PKCS8EncodedKeySpec(readFileBytes(filename))
+        val keyFactory = KeyFactory.getInstance("RSA")
+        return keyFactory.generatePrivate(keySpec)
+    }
+
+
+
+    fun encrypt(key :PublicKey, plaintext: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(plaintext);
+    }
+
+    fun decrypt(key :PrivateKey, ciphertext :ByteArray) :ByteArray {
+        val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(ciphertext);
+    }
 
 }
--- src/test/kotlin/com/davidbevan/encryption/TestEncryption.kt
+++ src/test/kotlin/com/davidbevan/encryption/TestEncryption.kt
@@ -1,11 +1,9 @@
 package com.davidbevan.encryption
 
 import io.kotlintest.specs.StringSpec
+import java.io.File
 import java.nio.charset.Charset
-import java.util.*
-import javax.crypto.SecretKey
 import javax.crypto.KeyGenerator
-
 
 
 class TestEncryption: StringSpec() {
@@ -13,27 +11,33 @@ class TestEncryption: StringSpec() {
 
 
     init {
-        "test encryption using Google KMS Symetric Key" {
+        "test encryption using Google KMS Symmetric Key" {
+            //WORKS FINE
+            //Just uses Google KMS symmetric key beauty_key-1 to encrypt and decrypt data.
             val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-1")
-
             val start = "Hello World"
             val encrypted = encryptionService.encrypt(start.toByteArray())
             val decrypted = String(encryptionService.decrypt(encrypted))
             printResult(start,encrypted,decrypted)
         }
 
-        "test decryption using Google KMS Symetric Key" {
-            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-1")
 
+        "test decryption using Google KMS symmetric Key" {
+            //WORKS FINE
+            //Uses the encrypted string from the symetric encyption test, decrypts it using beauty_key-1
+            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-1")
             val start = "Hello World"
             val encrypted = "0A24001DB11758F0B6FC4AC8DABC0825431A2536815A94D22391757723D691A1E8E97B539CB0123712350A0CB6AC05670D281D34FC2DB85F1213E793B9A0A876CFEFD61B83EB6E1C59C6D314291A101892F06D04B176777CB2C870395E5724".hexStringToByteArray()
             val decrypted = String(encryptionService.decrypt(encrypted))
             printResult(start,encrypted,decrypted)
         }
 
-        "test decryption using Google KMS asymmetric Key" {
-            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-asymmetric-decrypt")
 
+        "test decryption using Google KMS asymmetric Key" {
+            //WORKS FINE
+            // Covers scenario 1
+            //Trys to decrypt something that was encrypted using OpenSSL an the exported public key from beauty_key-asymmetric-decrypt
+            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-asymmetric-decrypt")
             val start = "Hello David!"
             //var encrypted was copy and pasted from hex of the file2.txt.enc which was file2.txt (Hello David!) encrypted using....
             //openssl pkeyutl -in file2.txt -encrypt -pubin -inkey beauty_crm_pcd-beauty_key-asymmetric-decrypt-1.pub -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256 -pkeyopt rsa_mgf1_md:sha256 > file2.txt.enc
@@ -42,11 +46,15 @@ class TestEncryption: StringSpec() {
             printResult(start,encrypted,decrypted)
         }
 
-        "test2 decryption using Google KMS asymmetric Key" {
-            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-asymmetric-decrypt")
 
+        "test2 decryption using Google KMS asymmetric Key" {
+            //WORKS FINE
+            //Covers scenario 1
+            //Repeat test with different data
+            //Trys to decrypt something that was encrypted using OpenSSL an the exported public key from beauty_key-asymmetric-decrypt
+            val encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-asymmetric-decrypt")
             val start = "Hello World!"
-            //var encrypted was copy and pasted from hex of the file.txt.enc which was file2.txt (Hello David!) encrypted using....
+            //var encrypted was copy and pasted from hex of the file.txt.enc which was file.txt (Hello David!) encrypted using....
             //openssl pkeyutl -in file.txt -encrypt -pubin -inkey beauty_crm_pcd-beauty_key-asymmetric-decrypt-1.pub -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256 -pkeyopt rsa_mgf1_md:sha256 > file.txt.enc
             val encrypted = "46C7552A22A0A5693AABA9DFF88434CC2F5929283252318561A6980EE1C35DA47DB0FB6A45B164A1707125E5684846DE263A7D016B526BCA3D25498A618BA4814BDFEC4983DB68E19DAD9853B4B57DF056FE3A918DE840ACF3051D5FD58760256E12485E43E285CCCCD73EBCFBA3F82374D620C23615ACAE5CAAD5A9C182F9B47367B9DD44CE524C116A76B5FE3CA36A2463923AF1C9B5F44211C01B374BA85A184F647E541973B4E74281E967E8CADC24DE795AC9E26C10249112F5729D0AC6E06C887108D5F89412898145608E6A7D29099C0A6EF117B0666FE55B406AEA7A566783979E354D082B8825D2C2DD9751901757918B8337A129C6D6570A2E7282899599DEFA3EA42EB9AB238A231BFBEC1EBE5C44EF1CD0D953516605AA45015B5DD6EAA717F46CACD963E607C57F56C359FAD4099BDAB4B94F9C06CA479D40F22ABC40E2FE68176FF944E42B090C59BEEA4B771AACE2E5FA6B21C0287C9C5AD1E8FFEF646957E9CB23A532348197A6F210B81DCC49F64B02A21A61BE8B19FADC".hexStringToByteArray()
             val decrypted = String(encryptionService.asymmetricDecrypt(encrypted))
@@ -54,25 +62,65 @@ class TestEncryption: StringSpec() {
         }
 
 
-        "test encryption using Google KMS" {
-            var encryptionService = EncryptionService("lee-jardine-sandbox", "europe-west2", "beauty_crm_pcd", "beauty_key-1")
-
+        "test encryption using AES" {
+            //WORKS FINE
+            //Just AES encryption - don't think this is of use
+            val keyGenerator = KeyGenerator.getInstance("AES")
+            keyGenerator.init(128)
+            val secretKey = keyGenerator.generateKey()
+            val encryptionService = AESEncryption(secretKey)
             val start = "Hello World"
             val encrypted = encryptionService.encrypt(start.toByteArray())
             val decrypted = String(encryptionService.decrypt(encrypted))
             printResult(start,encrypted,decrypted)
         }
 
-        "test encryption using AES" {
-            val keyGenerator = KeyGenerator.getInstance("AES")
-            keyGenerator.init(128)
-            val secretKey = keyGenerator.generateKey()
-            val encryptionService = AESEncryption(secretKey)
 
-            val start = "Hello World"
-            val encrypted = encryptionService.encrypt(start.toByteArray())
-            val decrypted = String(encryptionService.decrypt(encrypted))
-            printResult(start,encrypted,decrypted)
+        "test asymmetric encryption" {
+            //WORKS OK, BUT CAN'T DECRYPT
+            //Covers scenario 2
+            //This uses the beauty_key-asymmetric-decrypt public key to encrypt "Hello World!!!" and write it to a file
+            //It runs ok, but I can't seem to use the decrypt.sh (which uses gcloud) to decrypt the file.
+            val encryptionService = AsymmetricEncryptionService()
+            val dataString = "Hello World!!!"
+            val data = dataString.toByteArray()
+            //publicKeyString is the beauty_crm_pcd-beauty_key-asymmetric-decrypt-1.pub which was exported public ket from beauty_key-asymmetric-decrypt
+            val publicKeyString = "" +
+                    "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAu9ccvSSf3sd0zwEZdBKj\n" +
+                    "ESCDEekglF22l9jBzN68gL6aMImjOy8NP2VTCfvfoY3U+EmHMuTTk/7VFoUxsDa9\n" +
+                    "E6226Oqzh1m0ppkK3Kjeq+2aSNtqlngWsvQ8lqQO05offA1cYsigr4XwlqHse1mP\n" +
+                    "sxEmoJro/9C65tc8ylvgzS63pntUZ2149yjoUF50dHC2fGWqUTE8TQRL+16Qi4Ur\n" +
+                    "hMX47jeKxpOWFiTmUNoQwtTgEBtVBpfwLsk4+sOmCviQm1hRJtQLDiHdd4TIhHoT\n" +
+                    "d6OQnxKgJFgg6wMIQenywtJ5+CnwfHPVFIbwA9CVNGmPF8jpfKNwNHMdsTch+/D3\n" +
+                    "jMo8pm/EKsKpB9cXoM5BJ/zsCcEQPpRVaYgGDs3qVmgqct2TSN93bKVHHvQBZ8Ey\n" +
+                    "ngo/JRrQ7j1JGMhNghf3LSJJo4OoA9aEswjPUTpT3HeXHF0IKuLDMQdoJX+VAh+8\n" +
+                    "L3dVVN8UFPzgYACrWElzXDZaWSlSX9K6S9FrLkT0EPaZAgMBAAE="
+            println("\ndataString=\"$dataString\"")
+            println("\npublicKey=\"$publicKeyString\"")
+            val encryptedData = encryptionService.encrypt(data, publicKeyString)
+            print("\nencrypted=\"")
+            for (b in encryptedData) {
+                val st = String.format("%02X", b)
+                print(st)
+            }
+            print("\"\n")
+            File("/home/dabevan/Downloads/temp/encryption/kotlin.txt.enc").writeBytes(encryptedData)
+        }
+
+
+        "test other asymmetric encryption" {
+            //WORKS OK, BUT CAN'T DECRYPT
+            //This will use the public and private pems generated by OpenSSL to encrypt and decrypt the string "Hello World"
+            //and write the result into a file.
+            //The encryption and decryption seems to work, but I can't decrpt the file using OpenSSL and the private key
+            val encryptionService = EncryptionUsingExportedOpenSSLKeys()
+            val publicKey = encryptionService.readPublicKey("/home/dabevan/Downloads/temp/encryption/openSSLPublicKey.der")
+            val privateKey = encryptionService.readPrivateKey("/home/dabevan/Downloads/temp/encryption/openSSLPrivateKey.der")
+            val message = "Hello World".toByteArray(charset("UTF8"))
+            val secret = encryptionService.encrypt(publicKey, message)
+            File("/home/dabevan/Downloads/temp/encryption/another.txt.enc").writeBytes(secret)
+            val recovered_message = encryptionService.decrypt(privateKey, secret)
+            println(String(recovered_message, Charset.defaultCharset()))
         }
 
     }
