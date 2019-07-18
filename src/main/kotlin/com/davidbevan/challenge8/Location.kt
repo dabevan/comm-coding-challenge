7771d1119299f1e7dde688644d2f5af3704dc7bd
dabevan <dabevan@gmail.com>
2019-07-18 14:52:31 +0100

    Challenge 8 plus some tidy up of other projects

--- src/main/kotlin/com/davidbevan/challenge8/Location.kt
+++ src/main/kotlin/com/davidbevan/challenge8/Location.kt
@@ -1,29 +1,64 @@
 package com.davidbevan.challenge8
 
-abstract class Location {
-    var name = ""
-    open var collect = 0
-    var cost = 0
-    open var rent = 0
-    abstract var maxNumber: Int
 
+interface Purchasable {
+    val purchasePrice: Int
+    fun getOwner(): String?
+    fun purchase(purchaser: String)
 }
 
-
-class FreeParking: Location() {
-    override var maxNumber = 1
+interface Developable {
+    var shop:Shop?
+    fun build(newShop: Shop)
 }
 
+sealed class Location {
+    open fun getMaxNum() = 0
+    open fun getRent() = 0
+    open fun getPassingPayment() = 0
 
-class Go: Location() {
-    override var maxNumber = 1
-    override var collect = 100
+
+    class FreeParking : Location() {
+        override fun getMaxNum() = 1
+    }
+
+
+    class Go : Location() {
+        override fun getMaxNum() = 1
+        override fun getPassingPayment() = 100
+    }
+
+
+    class FactoryOrWarehouse(val name: String, override val purchasePrice: Int) :Purchasable, Location() {
+        private var owner:String? = null
+        override fun getOwner() = owner
+        override fun getMaxNum() = 4
+        override fun getRent() = if (getOwner() != null) { 20 } else {0}
+        override fun purchase(purchaser: String) { owner = purchaser }
+    }
+
+
+    class RetailSite(val name: String,
+                     override val purchasePrice: Int,
+                     val undevelopedRent: Int,
+                     val costOfMiniStore: Int,
+                     val costOfSupermarket: Int,
+                     val costOfMegaStore: Int) :Purchasable, Developable, Location() {
+        private var owner:String? = null
+        override fun getOwner() = owner
+        override fun getMaxNum() = 20
+        override fun getRent() = if (getOwner() != null) { undevelopedRent + (shop?.getRent() ?: 0) } else {0}
+        override fun purchase(purchaser: String) { owner = purchaser }
+        override var shop: Shop? = null
+        override fun build(newShop: Shop) { shop = newShop }
+
+    }
 }
 
-
-class Factory: Location() {
-    override var maxNumber = 4
-    override var collect = 100
-    override var rent = 20
-
+abstract class Shop(private val additionalRent:Int) {
+    fun getRent() = additionalRent
 }
+
+class MiniStore(private val additionalRent:Int): Shop(additionalRent)
+class Supermarket(private val additionalRent:Int): Shop(additionalRent)
+class MegaStore(private val additionalRent:Int): Shop(additionalRent)
--- src/test/kotlin/com/davidbevan/challenge8/TestLocations.kt
+++ src/test/kotlin/com/davidbevan/challenge8/TestLocations.kt
@@ -1,19 +1,85 @@
-//package com.davidbevan.challenge8
-//
-//import io.kotlintest.matchers.*
-//import io.kotlintest.specs.StringSpec
-//
-//
-//class	TestLocations() :	StringSpec()	{
-//    init {
-//
-//        var board = Board()
-//
-//        "test create go" {
-//            Board.add(Go(), 100)
-//            Board.get(100) shouldBe
-//        }
-//    }
-//}
-//
-//
+package com.davidbevan.challenge8
+
+import io.kotlintest.matchers.*
+import io.kotlintest.specs.StringSpec
+
+
+class	TestLocations() :	StringSpec()	{
+    init {
+
+        "test Free Parking" {
+            var freeParking = Location.FreeParking()
+            freeParking.getMaxNum() shouldBe (1)
+            freeParking.getRent() shouldBe (0)
+            freeParking.getPassingPayment() shouldBe (0)
+        }
+
+        "test Go" {
+            var go = Location.Go()
+            go.getMaxNum() shouldBe (1)
+            go.getRent() shouldBe (0)
+            go.getPassingPayment() shouldBe (100)
+        }
+
+        "test Factory or Warehouse without owner" {
+            var factoryOrWarehouse = Location.FactoryOrWarehouse("A factory name", 1000)
+            factoryOrWarehouse.getMaxNum() shouldBe (4)
+            factoryOrWarehouse.getRent() shouldBe (0)
+            factoryOrWarehouse.getPassingPayment() shouldBe (0)
+        }
+
+        "test Factory or Warehouse with owner" {
+            var factoryOrWarehouse = Location.FactoryOrWarehouse("A factory name", 1000)
+            factoryOrWarehouse.purchase("Dave")
+            factoryOrWarehouse.getMaxNum() shouldBe (4)
+            factoryOrWarehouse.getRent() shouldBe (20)
+            factoryOrWarehouse.getPassingPayment() shouldBe (0)
+        }
+
+        "test Retail Site without owner" {
+            var retailSite = Location.RetailSite("A retail site name", 1000, 30, 100,150,200)
+            retailSite.getMaxNum() shouldBe (20)
+            retailSite.getRent() shouldBe (0)
+            retailSite.getPassingPayment() shouldBe (0)
+        }
+
+
+        "test undeveloped Retail Site with owner" {
+            var retailSite = Location.RetailSite("A retail site name", 1000, 30, 100,150,200)
+            retailSite.purchase("Dave")
+            retailSite.getMaxNum() shouldBe (20)
+            retailSite.getRent() shouldBe (30)
+            retailSite.getPassingPayment() shouldBe (0)
+        }
+
+
+        "test developed Retail Site with owner and a Mini Store" {
+            var retailSite = Location.RetailSite("A retail site name", 1000, 30, 100,150,200)
+            retailSite.purchase("Dave")
+            retailSite.build(MiniStore(30))
+            retailSite.getMaxNum() shouldBe (20)
+            retailSite.getRent() shouldBe (60)
+            retailSite.getPassingPayment() shouldBe (0)
+        }
+
+        "test developed Retail Site with owner and a Supermarket" {
+            var retailSite = Location.RetailSite("A retail site name", 1000, 30, 100,150,200)
+            retailSite.purchase("Dave")
+            retailSite.build(Supermarket(40))
+            retailSite.getMaxNum() shouldBe (20)
+            retailSite.getRent() shouldBe (70)
+            retailSite.getPassingPayment() shouldBe (0)
+        }
+
+        "test developed Retail Site with owner and a Mega Store" {
+            var retailSite = Location.RetailSite("A retail site name", 1000, 30, 100,150,200)
+            retailSite.purchase("Dave")
+            retailSite.build(Supermarket(50))
+            retailSite.getMaxNum() shouldBe (20)
+            retailSite.getRent() shouldBe (80)
+            retailSite.getPassingPayment() shouldBe (0)
+        }
+    }
+}
+
+
