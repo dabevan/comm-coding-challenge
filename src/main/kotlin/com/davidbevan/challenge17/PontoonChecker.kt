02cc05118becae5e3fabfe36b9078d82d273096c
dabevan <dabevan@gmail.com>
2019-11-20 13:07:45 +0000

    Add picture and non picture tests

---
+++ src/main/kotlin/com/davidbevan/challenge17/PontoonChecker.kt
@@ -0,0 +1,20 @@
+package com.davidbevan.challenge17
+
+fun isHandPontoon(hand: List<String>): Boolean {
+    if (hand.size != 2) return false
+    if (isCardAPicture(hand[0]) && isCardAnAce(hand[1])) return true
+    if (isCardAnAce(hand[0]) && isCardAPicture(hand[1])) return true
+    return false
+}
+
+fun isCardAPicture(card: String): Boolean {
+    if (card.startsWith('K')) return true
+    if (card.startsWith('Q')) return true
+    if (card.startsWith('J')) return true
+    return false
+}
+
+fun isCardAnAce(card: String): Boolean {
+    if (card.startsWith('A')) return true
+    return false
+}
