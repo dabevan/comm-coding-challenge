e9a7752fcb5a0cf4937d3bcafbca031e7fc824ef
dabevan <dabevan@gmail.com>
2020-01-23 15:11:30 +0000

    All tests working

--- src/main/kotlin/com/davidbevan/challenge19/Trolley.kt
+++ src/main/kotlin/com/davidbevan/challenge19/Trolley.kt
@@ -1,117 +1,95 @@
 package com.davidbevan.challenge19
 
 import java.io.UnsupportedEncodingException
-import java.nio.charset.Charset
 import java.util.*
 import kotlin.math.roundToInt
 
 
-class Trolley {
-
-    val storeMap =
-            "**************************\n" +
-            "*                        *\n" +
-            "* ********** *********** *\n" +
-            "* ********** *********** *\n" +
-            "*                        *\n" +
-            "* ********** *********** *\n" +
-            "* ********** *********** *\n" +
-            "* ********** *********** *\n" +
-            "*                        *\n" +
-            "**************************\n"
-
-    var storeMapList = emptyList<String>()
-
-    init {
-        storeMapList = storeMap.split("\n")
+fun move(command: String? = null, referenceId: String? = null): TrolleyView {
+    var position = Position(getXFromReferenceId(referenceId),getYFromReferenceId(referenceId))
+    var direction = getDirectionFromReferenceId(referenceId)
+    val trolleyId = getTrolleyIdFromReferenceId(referenceId)
+    when (command) {
+        "M" -> position = position.moveOneSquare(direction)
+        "L" -> direction = rotateTrolleyLeft(direction)
+        "R" -> direction = rotateTrolleyRight(direction)
     }
-
-
-    
-    fun move(command: String? = null, referenceId: String? = null): TrolleyView {
-        var position = Position(getXFromReferenceId(referenceId),getYFromReferenceId(referenceId))
-        var direction = getDirectionFromReferenceId(referenceId)
-        val trolleyId = getTrolleyIdFromReferenceId(referenceId)
-        when {
-            command == null -> position = position.moveOneSquare(direction)
-            command == "M" -> position = position.moveOneSquare(direction)
-            command == "L" -> direction = rotateTrolleyLeft(direction)
-            command == "R" -> direction = rotateTrolleyRight(direction)
-        }
-        return TrolleyView(calculateView(position,direction), encodeReferenceId(position.x, position.y, direction, trolleyId))
-    }
-
-    fun calculateView(position: Position,direction: String): MutableList<String> {
-        var viewAhead = mutableListOf<String>()
-        var currentSquareAndAdjacents = getSquareAndAdjacentsAtPosition(position, direction)
-        var viewPosition = position
-        while(currentSquareAndAdjacents.getMiddleSquare() != "*") {
-            viewPosition = viewPosition.moveOneSquare(direction)
-            currentSquareAndAdjacents = getSquareAndAdjacentsAtPosition(viewPosition, direction)
-            viewAhead.add(currentSquareAndAdjacents.convertToDirectionOptions())
-        }
-        return viewAhead
-    }
-
-    fun getSquareAndAdjacentsAtPosition(position: Position, direction: String): String {
-        return when(direction){
-            "N" -> getSquareAtPosition(Position(position.x-1,position.y)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x+1,position.y))
-            "S" -> getSquareAtPosition(Position(position.x+1,position.y)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x-1,position.y))
-            "E" -> getSquareAtPosition(Position(position.x,position.y-1)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x,position.y+1))
-            "W" -> getSquareAtPosition(Position(position.x,position.y+1)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x,position.y-1))
-            else -> ""
-        }
-    }
-
-    fun getSquareAtPosition(position: Position): String {
-        return storeMapList[position.y].subSequence(position.x,position.x+1).toString()
-    }
-
-    fun rotateTrolleyLeft(direction:String): String {
-        return when {
-            direction == "N" -> "W"
-            direction == "E" -> "N"
-            direction == "S" -> "E"
-            direction == "W" -> "S"
-            else -> "E"
-        }
-    }
-
-    fun rotateTrolleyRight(direction:String): String {
-        return when (direction) {
-            "N" -> "E"
-            "E" -> "S"
-            "S" -> "W"
-            "W" -> "N"
-            else -> "E"
-        }
-    }
-
-    
-    fun getXFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringBefore(",").toInt()
-
-    
-    fun getYFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter(",").substringBefore(":").toInt()
-
-    
-    fun getDirectionFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter(":").substringBefore("#")
-
-    
-    fun getTrolleyIdFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter("#")
-
-    fun newTrolleyId() = (Math.random() * 999999).roundToInt().toString()
-
-    
-    fun decodeReferenceId(referenceId: String?): String {
-        if (referenceId == null) return "1,1:E#${newTrolleyId()}"
-        return decode(referenceId)
-    }
-
-    fun encodeReferenceId(x: Int, y: Int, direction: String, trolleyId: String): String {
-        return encode("$x,$y:$direction#$trolleyId")
-    }
-
+    return TrolleyView(calculateView(position,direction), encodeReferenceId(position.x, position.y, direction, trolleyId))
 }
+
+
+fun calculateView(position: Position,direction: String): MutableList<String> {
+    var viewAhead = mutableListOf<String>()
+    var currentSquareAndAdjacents = getSquareAndAdjacentsAtPosition(position, direction)
+    var viewPosition = position
+    while(currentSquareAndAdjacents.getMiddleSquare() != "*") {
+        viewPosition = viewPosition.moveOneSquare(direction)
+        currentSquareAndAdjacents = getSquareAndAdjacentsAtPosition(viewPosition, direction)
+        viewAhead.add(currentSquareAndAdjacents.convertToDirectionOptions())
+    }
+    return viewAhead
+}
+
+
+fun getSquareAndAdjacentsAtPosition(position: Position, direction: String): String {
+    return when(direction){
+        "N" -> getSquareAtPosition(Position(position.x-1,position.y)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x+1,position.y))
+        "S" -> getSquareAtPosition(Position(position.x+1,position.y)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x-1,position.y))
+        "E" -> getSquareAtPosition(Position(position.x,position.y-1)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x,position.y+1))
+        "W" -> getSquareAtPosition(Position(position.x,position.y+1)) + getSquareAtPosition(position) + getSquareAtPosition(Position(position.x,position.y-1))
+        else -> ""
+    }
+}
+
+
+fun getSquareAtPosition(position: Position): String {
+    return StoreLayout().storeMapList[position.y].subSequence(position.x,position.x+1).toString()
+}
+
+
+fun rotateTrolleyLeft(direction:String): String {
+    return when {
+        direction == "N" -> "W"
+        direction == "E" -> "N"
+        direction == "S" -> "E"
+        direction == "W" -> "S"
+        else -> "E"
+    }
+}
+
+
+fun rotateTrolleyRight(direction:String): String {
+    return when (direction) {
+        "N" -> "E"
+        "E" -> "S"
+        "S" -> "W"
+        "W" -> "N"
+        else -> "E"
+    }
+}
+
+
+fun getXFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringBefore(",").toInt()
+
+fun getYFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter(",").substringBefore(":").toInt()
+
+fun getDirectionFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter(":").substringBefore("#")
+
+fun getTrolleyIdFromReferenceId(referenceId: String?) = decodeReferenceId(referenceId).substringAfter("#")
+
+fun newTrolleyId() = (Math.random() * 999999).roundToInt().toString()
+
+
+fun decodeReferenceId(referenceId: String?): String {
+    if (referenceId == null) return "1,1:E#${newTrolleyId()}"
+    return decode(referenceId)
+}
+
+
+fun encodeReferenceId(x: Int, y: Int, direction: String, trolleyId: String): String {
+    return encode("$x,$y:$direction#$trolleyId")
+}
+
 
 data class Position(val x: Int,val y: Int) {
     fun moveOneSquare(direction:String): Position {
@@ -130,7 +108,6 @@ data class TrolleyView(val viewAhead: List<String>, val referenceId: String)
 fun String.getMiddleSquare(): String = this.substring(1,2)
 
 fun String.convertToDirectionOptions(): String {
-
     return when(this) {
         "***" -> ""
         "* *" -> "O"
@@ -142,15 +119,14 @@ fun String.convertToDirectionOptions(): String {
 }
 
 
-
 @UseExperimental(ExperimentalStdlibApi::class)
 fun decode(encodedText: String): String {
     return Base64.getDecoder().decode(encodedText).decodeToString()
 }
 
+
 fun encode(plainText: String): String {
     var data = ByteArray(0)
-
     try {
         data = plainText.toByteArray()
     } catch (e: UnsupportedEncodingException) {
@@ -161,26 +137,22 @@ fun encode(plainText: String): String {
 }
 
 
-fun main() {
-    var x = 0
-    var y = 0
-    while(y < 10) {
-        while(x < 26) {
-            print(Trolley().getSquareAtPosition(Position(x,y)))
-            x++
-        }
-        x=0
-        y++
-        print("\n")
+class StoreLayout() {
+    val storeMap =
+            "**************************\n" +
+            "*                        *\n" +
+            "* ********** *********** *\n" +
+            "* ********** *********** *\n" +
+            "*                        *\n" +
+            "* ********** *********** *\n" +
+            "* ********** *********** *\n" +
+            "* ********** *********** *\n" +
+            "*                        *\n" +
+            "**************************\n"
+
+    var storeMapList = emptyList<String>()
+
+    init {
+        storeMapList = storeMap.split("\n")
     }
-
-    println("1,1,E |${Trolley().getSquareAndAdjacentsAtPosition(Position(1,1),"E")}|")
-    println("1,1,W |${Trolley().getSquareAndAdjacentsAtPosition(Position(1,1),"W")}|")
-    println("2,1,E |${Trolley().getSquareAndAdjacentsAtPosition(Position(2,1),"E")}|")
-    println("2,1,W |${Trolley().getSquareAndAdjacentsAtPosition(Position(2,1),"W")}|")
-    println("2,1,S |${Trolley().getSquareAndAdjacentsAtPosition(Position(2,1),"S")}|")
-    println("2,1,N |${Trolley().getSquareAndAdjacentsAtPosition(Position(2,1),"N")}|")
-    println("1,2,S |${Trolley().getSquareAndAdjacentsAtPosition(Position(1,2),"S")}|")
-    println("1,2,N |${Trolley().getSquareAndAdjacentsAtPosition(Position(1,2),"N")}|")
 }
-
--- src/test/kotlin/com/davidbevan/challenge19/TestTrolley.kt
+++ src/test/kotlin/com/davidbevan/challenge19/TestTrolley.kt
@@ -8,78 +8,76 @@ class TestTrolley {
 
     @Test
     fun testMovement() {
-        val trolley = Trolley()
-        var trolleyView = trolley.move()
+        var trolleyView = move()
+        Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", ""))
+
+        trolleyView = move("M", trolleyView.referenceId)
         Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", ""))
-        println(trolleyView.referenceId)
 
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "O", "O", "O", "O", "O", "OR", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", ""))
-
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
         Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OR", ""))
 
-        trolleyView = trolley.move("R", trolleyView.referenceId)
+        trolleyView = move("R", trolleyView.referenceId)
         Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "OLR", "O", "O", "O", "OLR", ""))
 
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
-        trolleyView = trolley.move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
+        trolleyView = move("M", trolleyView.referenceId)
         Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "OLR", ""))
 
-        trolleyView = trolley.move("L", trolleyView.referenceId)
+        trolleyView = move("L", trolleyView.referenceId)
         Assertions.assertThat(trolleyView.viewAhead).isEqualTo(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "OLR", ""))
      }
 
+
     @Test
     fun testGetSquare() {
-        val trolley = Trolley()
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(0,0))).isEqualTo("*")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(1,1))).isEqualTo(" ")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(2,1))).isEqualTo(" ")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(2,2))).isEqualTo("*")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(2,3))).isEqualTo("*")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(2,4))).isEqualTo(" ")
-        Assertions.assertThat(trolley.getSquareAtPosition(Position(2,5))).isEqualTo("*")
+        Assertions.assertThat(getSquareAtPosition(Position(0,0))).isEqualTo("*")
+        Assertions.assertThat(getSquareAtPosition(Position(1,1))).isEqualTo(" ")
+        Assertions.assertThat(getSquareAtPosition(Position(2,1))).isEqualTo(" ")
+        Assertions.assertThat(getSquareAtPosition(Position(2,2))).isEqualTo("*")
+        Assertions.assertThat(getSquareAtPosition(Position(2,3))).isEqualTo("*")
+        Assertions.assertThat(getSquareAtPosition(Position(2,4))).isEqualTo(" ")
+        Assertions.assertThat(getSquareAtPosition(Position(2,5))).isEqualTo("*")
     }
+
 
     @Test
     fun testGetSquareAndAdjacents() {
-        val trolley = Trolley()
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(1,1),"E")).isEqualTo("*  ")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(1,1),"W")).isEqualTo("  *")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(2,1),"E")).isEqualTo("* *")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(2,1),"W")).isEqualTo("* *")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(2,1),"S")).isEqualTo("   ")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(2,1),"N")).isEqualTo("   ")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(1,2),"S")).isEqualTo("* *")
-        Assertions.assertThat(trolley.getSquareAndAdjacentsAtPosition(Position(1,2),"N")).isEqualTo("* *")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(1,1),"E")).isEqualTo("*  ")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(1,1),"W")).isEqualTo("  *")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(2,1),"E")).isEqualTo("* *")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(2,1),"W")).isEqualTo("* *")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(2,1),"S")).isEqualTo("   ")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(2,1),"N")).isEqualTo("   ")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(1,2),"S")).isEqualTo("* *")
+        Assertions.assertThat(getSquareAndAdjacentsAtPosition(Position(1,2),"N")).isEqualTo("* *")
     }
 
 
     @Test
     fun testRotateLeft() {
-        val trolley = Trolley()
-        Assertions.assertThat(trolley.rotateTrolleyLeft("N")).isEqualTo("W")
-        Assertions.assertThat(trolley.rotateTrolleyLeft("E")).isEqualTo("N")
-        Assertions.assertThat(trolley.rotateTrolleyLeft("S")).isEqualTo("E")
-        Assertions.assertThat(trolley.rotateTrolleyLeft("W")).isEqualTo("S")
+        Assertions.assertThat(rotateTrolleyLeft("N")).isEqualTo("W")
+        Assertions.assertThat(rotateTrolleyLeft("E")).isEqualTo("N")
+        Assertions.assertThat(rotateTrolleyLeft("S")).isEqualTo("E")
+        Assertions.assertThat(rotateTrolleyLeft("W")).isEqualTo("S")
     }
 
+    
     @Test
     fun testRotateRight() {
-        val trolley = Trolley()
-        Assertions.assertThat(trolley.rotateTrolleyRight("N")).isEqualTo("E")
-        Assertions.assertThat(trolley.rotateTrolleyRight("E")).isEqualTo("S")
-        Assertions.assertThat(trolley.rotateTrolleyRight("S")).isEqualTo("W")
-        Assertions.assertThat(trolley.rotateTrolleyRight("W")).isEqualTo("N")
+        Assertions.assertThat(rotateTrolleyRight("N")).isEqualTo("E")
+        Assertions.assertThat(rotateTrolleyRight("E")).isEqualTo("S")
+        Assertions.assertThat(rotateTrolleyRight("S")).isEqualTo("W")
+        Assertions.assertThat(rotateTrolleyRight("W")).isEqualTo("N")
     }
 }
