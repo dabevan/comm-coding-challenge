c30859db90c97cbb47902a869e6daaf2d60dd3c6
dabevan <dabevan@gmail.com>
2020-01-24 15:55:35 +0000

    Working maze walker

--- src/main/kotlin/com/davidbevan/codingchallengeevent/Client.kt
+++ src/main/kotlin/com/davidbevan/codingchallengeevent/Client.kt
@@ -1,9 +1,12 @@
 package com.davidbevan.codingchallengeevent
 
+import com.davidbevan.codingchallengeevent.Direction.*
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
+
+var numberOfSteps = 0
 
 class Client {
 
@@ -11,6 +14,7 @@ class Client {
         val request = Request(Method.GET, url)
         val client: HttpHandler = JavaHttpClient()
         println(url)
+        numberOfSteps++
         return client(request).bodyString()
     }
 
@@ -20,7 +24,46 @@ class Client {
 
 }
 
+val listOfPositions = mutableListOf(Position(1, 1))
 val listOfVisitedReferenceIds = mutableListOf<String>()
+var direction: Direction = EAST
+
+data class Position(val x: Int, val y: Int) {
+    fun moveOneSquare(direction: Direction): Position {
+        return when (direction) {
+            NORTH -> Position(x, y - 1)
+            EAST -> Position(x + 1, y)
+            SOUTH -> Position(x, y + 1)
+            WEST -> Position(x - 1, y)
+        }
+    }
+}
+
+enum class Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
+
+    fun turn(command: String): Direction {
+        return when (command) {
+            "L" -> when (this) {
+                NORTH -> WEST
+                SOUTH -> EAST
+                EAST -> NORTH
+                WEST -> SOUTH
+            }
+            "R" -> when (this) {
+                NORTH -> EAST
+                SOUTH -> WEST
+                EAST -> SOUTH
+                WEST -> NORTH
+            }
+            else -> this
+        }
+    }
+}
+
 
 fun main() {
     val url = "https://challenge20.appspot.com"
@@ -32,11 +75,13 @@ fun main() {
     )
 
     walkMaze(whatToDoNext(firstRefAndSquare), firstRefAndSquare.referenceid)
-    println("FINISHED")
-
+    //println("FINISHED in $numberOfSteps steps")
+//    println(listOfPositions)
+    drawRoute()
 }
 
 fun walkMaze(command: String, referenceid: String) {
+    drawRoute()
     var currentRef = referenceid
     lateinit var refAndNextSquare: RefAndNextSquare
 
@@ -45,6 +90,7 @@ fun walkMaze(command: String, referenceid: String) {
         .forEach {
             refAndNextSquare = getRefAndNextSquare(it.toString(), currentRef)
             currentRef = refAndNextSquare.referenceid
+            recordBreadcrumbs(it.toString())
         }
 
     val command = whatToDoNext(refAndNextSquare) //.also { println("""$refAndNextSquare Next Command: $it""") }
@@ -71,10 +117,43 @@ fun whatToDoNext(referenceAndNextSquare: RefAndNextSquare): String {
         listOfVisitedReferenceIds.clear()
         return "L"
     }
+//    return when (referenceAndNextSquare.nextSquare) {
+//        "O", "OL" -> "M"
+//        "OR", "OLR" -> "MR"
+//        "X" -> ""
+//        else -> "L"
+//    }
     return when (referenceAndNextSquare.nextSquare) {
-        "O", "OL" -> "M"
-        "OR", "OLR" -> "MR"
+        "O", "OR" -> "M"
+        "OL", "OLR" -> "ML"
         "X" -> ""
-        else -> "L"
+        else -> "R"
     }
 }
+
+fun recordBreadcrumbs(command: String) {
+    when (command) {
+        "M" -> listOfPositions.add(listOfPositions.last().moveOneSquare(direction))
+        else -> direction = direction.turn(command)
+    }
+}
+
+fun drawRoute() {
+    fun emptyLine() = (1..20).map { "   " }.toMutableList()
+    val grid = (1..20).map { emptyLine() }.toMutableList()
+
+    listOfPositions.forEachIndexed { index, position ->
+        grid[position.y][position.x] = index.toString().padStart(3, ' ')
+    }
+
+    Thread.sleep(1000)
+    println()
+    print(grid.map {
+        it.joinToString("")
+    }.joinToString("\n"))
+}
+
+
+
+
+
